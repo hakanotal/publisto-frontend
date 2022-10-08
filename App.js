b3998307bbc05dcfac3edcdd86af178760d17f87
Halil Faruk Karagöz <karagozh18@itu.edu.tr>
2022-10-08 14:24:47 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginPage from "/src/components/LoginPage"
+export default function App() {
+  return (
+    
+   <LoginPage />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
