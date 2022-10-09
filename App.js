c5c03112c4492905c40b51ea0630ab42425a7579
Halil Faruk Karagöz <karagozh18@itu.edu.tr>
2022-10-09 13:40:13 +0300

    some progress

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginPage from './src/components/LoginPage';
 export default function App() {
+  let userToken
   return (
    <View>
     <LoginPage/>
-
    </View>
   );
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -11,10 +11,69 @@ import {
   SafeAreaView
 } from "react-native";
 
+const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    return (
+        <SafeAreaView>
+
+            <Image source={require('./cat.webp')} style= {styles.image}/>
+
+            <SafeAreaView style={styles.container}>
+
+                <StatusBar style="auto" />
+                <SafeAreaView style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Email."
+                    placeholderTextColor="#003f5c"
+                    onChangeText={(email) => setEmail(email)}
+                />
+                </SafeAreaView>
+        
+            <SafeAreaView style={styles.inputView}>
+                <TextInput
+                style={styles.TextInput}
+                placeholder="Password."
+                placeholderTextColor="#003f5c"
+                secureTextEntry={true}
+                onChangeText={(password) => setPassword(password)}
+                />
+            </SafeAreaView>
+        
+            <TouchableOpacity>
+                <Text style={styles.forgot_button}>Forgot Password?</Text>
+                
+            </TouchableOpacity>
+        
+            <TouchableOpacity style={styles.loginBtn}>
+                <Text style={styles.loginText}>LOGIN</Text>
+            </TouchableOpacity>
+            </SafeAreaView>
+        </SafeAreaView>
+        );
+    }
+
 
 const styles = StyleSheet.create({
+    image: {
+        alignContent: "center",
+        top : "50%",
+        left : 40,
+        width : 300,
+        height : 200
+    }
+    ,
+
     container: {
     flex: 1,
+    backgroundColor : "",
+    top : "90%",
+    width : "100%",
+    height : "80%",
+    display : "flex",
+    alignContent : "center",
+    padding : 20,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
